fa4ad7eb0468caf31a61b9e22b1495169623421a
AntoineOrsoni <ant.orsoni@gmail.com>
2020-01-26 09:02:16 +0100

    first commit

---
+++ {{cookiecutter.python_file}}.py
@@ -0,0 +1,24 @@
+import jinja2
+import yaml
+
+# Where's the folder with my templates
+template_loader = jinja2.FileSystemLoader(searchpath="./templates")
+template_env = jinja2.Environment(loader=template_loader)
+
+# Which file is my template
+{{template_file}} = "{{cookiecutter.template_file}}.tpl"
+template = template_env.get_template({{template_file}})
+
+# Loading my variables, stored in a YAML file
+with open("{{cookiecutter.variables_file}}.yaml", 'r') as variables_file:
+    variables = yaml.safe_load(variables_file)
+
+    outputText = ""
+    # Generating the output for BundleEthernet interfaces
+    for element in range(len(variables)):
+        # TODO
+
+# Saving the output in a text file
+# Content of the file gets removed every time. Then content is added.
+with open("./outputs/{{cookiecutter.output_file}}.txt", "w") as output_file:
+    print(outputText, file=output_file)
