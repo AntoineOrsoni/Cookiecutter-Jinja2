b2ace62bfef3e8e370ebbfcb55125cf61c158321
AntoineOrsoni <ant.orsoni@gmail.com>
2020-02-17 17:32:40 +0100

    bug fix

--- {{cookiecutter.project_name}}/{{cookiecutter.python_file}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.python_file}}.py
@@ -2,7 +2,7 @@ import jinja2
 import yaml
 
 # Where's the folder with my templates (or my folders, if multiple)
-template_loader = jinja2.FileSystemLoader(searchpath="./{{cookiecutter.dir_template}}")
+template_loader = jinja2.FileSystemLoader(searchpath="./{{cookiecutter.template_dir}}")
 
 # Instance of the Environment class. Gives the loader (above), optionally parameters like
 # block strings '{% %}', variable strings etc.
