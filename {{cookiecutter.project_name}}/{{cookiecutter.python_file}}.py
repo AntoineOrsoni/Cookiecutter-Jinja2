1bb8046a4e3e7871d224b3e3b5251f7e7c85b24b
AntoineOrsoni <ant.orsoni@gmail.com>
2020-02-17 16:42:24 +0100

    bug fix

--- {{cookiecutter.project_name}}/{{cookiecutter.python_file}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.python_file}}.py
@@ -1,13 +1,15 @@
 import jinja2
 import yaml
 
-# Where's the folder with my templates
+# Where's the folder with my templates (or my folders, if multiple)
 template_loader = jinja2.FileSystemLoader(searchpath="./{{cookiecutter.dir_template}}")
+
+# Instance of the Environment class. Gives the loader (above), optionally parameters like
+# block strings '{% %}', variable strings etc.
 template_env = jinja2.Environment(loader=template_loader)
 
 # Which file is my template
-{{cookiecutter.template_file}} = "{{cookiecutter.template_file}}.tpl"
-template = template_env.get_template({{cookiecutter.template_file}})
+template = template_env.get_template("{{cookiecutter.template_file}}.tpl")
 
 # Loading my variables, stored in a YAML file
 with open("{{cookiecutter.variables_file}}.yaml", 'r') as variables_file:
